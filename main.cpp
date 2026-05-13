6c4e06d3809d2f08f3628c5600b2f0a53de38b2e
cedrexpelagio <cedrexpelagio@gmail.com>
2026-05-13 10:23:13 +0800

    Added selectCompany and selectPlatoon function
    Added this functions to prevent human error by using a numbered selection menu instead of free text input. Also, added example in the registerUser function for the user

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 const int MAX_CADETS = 1000;
 
 // Basic Cadets struct
-struct BasicCadets { 
+struct BasicCadets
+{
     string userName = "";
     string studentID = "";
     string program = "";
@@ -19,30 +20,93 @@ struct BasicCadets {
     int numExcuse = 0;
 };
 
-// Function to register a user
-void registerUser (BasicCadets *cadets, int *index, int *numberOfCadets) {
-    cout << "User Number " << *index + 1 << ":" << endl;
-    cout << "User Name: ";
-    getline (cin, (cadets + *index)->userName);
-    cout << "Student ID: ";
-    getline (cin, (cadets + *index)->studentID);
-    cout << "Program: ";
-    getline (cin, (cadets + *index)->program);
-    cout << "Company: ";
-    getline (cin, (cadets + *index)->company);
-    cout << "Platoon: ";
-    getline (cin, (cadets + *index)->platoon);
-    *index++;
-    *numberOfCadets++;
+// Function to select a company
+void selectCompany(BasicCadets *cadets, int *index)
+{
+    int companyChoice;
+    cout << "Company:" << endl;
+    cout << "1. Alpha" << endl;
+    cout << "2. Bravo" << endl;
+    cout << "3. Charlie" << endl;
+    cout << "4. Delta" << endl;
+    cout << "Choice: ";
+    cin >> companyChoice;
+
+    switch (companyChoice)
+    {
+    case 1:
+        (cadets + *index)->company = "Alpha";
+        break;
+    case 2:
+        (cadets + *index)->company = "Bravo";
+        break;
+    case 3:
+        (cadets + *index)->company = "Charlie";
+        break;
+    case 4:
+        (cadets + *index)->company = "Delta";
+        break;
+    default:
+        cout << "Invalid choice!" << endl;
+    }
 }
 
+// Function to select a platoon
+void selectPlatoon(BasicCadets *cadets, int *index)
+{
+    int platoonChoice;
+    cout << "Platoon:" << endl;
+    cout << "1. 1st Platoon" << endl;
+    cout << "2. 2nd Platoon" << endl;
+    cout << "3. 3rd Platoon" << endl;
+    cout << "4. 4th Platoon" << endl;
+    cout << "Choice: ";
+    cin >> platoonChoice;
 
-int main (){
-    int numberOfCadets = 0; // total number of cadets registered
-    int lastCadetIndex = 0; // track the last index of user
+    switch (platoonChoice)
+    {
+    case 1:
+        (cadets + *index)->platoon = "1st Platoon";
+        break;
+    case 2:
+        (cadets + *index)->platoon = "2nd Platoon";
+        break;
+    case 3:
+        (cadets + *index)->platoon = "3rd Platoon";
+        break;
+    case 4:
+        (cadets + *index)->platoon = "4th Platoon";
+        break;
+    default:
+        cout << "Invalid choice!" << endl;
+    }
+}
+
+// Function to register a user
+void registerUser(BasicCadets *cadets, int *index, int *numberOfCadets)
+{
+    cout << "User Number " << *index + 1 << ":" << endl;
+    cout << "Full Name\n(ex. CRUZ JOSE A): ";
+    getline(cin, (cadets + *index)->userName);
+    cout << "Student ID\n(ex. LQ-00093-2025): ";
+    getline(cin, (cadets + *index)->studentID);
+    cout << "Program\n(ex. BSCE-1-1): ";
+    getline(cin, (cadets + *index)->program);
+  
+    selectCompany(cadets, index);
+    selectPlatoon(cadets, index);
+
+    (*index)++;
+    (*numberOfCadets)++;
+}
+
+int main()
+{
+    int numberOfCadets = 0;                            // total number of cadets registered
+    int lastCadetIndex = 0;                            // track the last index of user
     BasicCadets *cadets = new BasicCadets[MAX_CADETS]; // dynamic array to store cadets
 
-    registerUser(cadets, &lastCadetIndex, &numberOfCadets); 
+    registerUser(cadets, &lastCadetIndex, &numberOfCadets);
 
     delete[] cadets;
     return 0;
