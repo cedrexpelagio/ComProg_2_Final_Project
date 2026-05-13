fa4f7b682d3dadd71c719220c8183130df91fbb3
cedrexpelagio <cedrexpelagio@gmail.com>
2026-05-13 09:10:16 +0800

    Added BasicCadets struct and registerUser function
    Added struct for basic cadet information including userName, studentID, program, company, and platoon. Added registerUser function to handle cadet registration input.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 // UnitSync
 
-int main (){
-    
+const int MAX_CADETS = 1000;
 
+// Basic Cadets struct
+struct BasicCadets { 
+    string userName = "";
+    string studentID = "";
+    string program = "";
+    string company = "";
+    string platoon = "";
+    string status = "";
+    int numPresent = 0;
+    int numAbsent = 0;
+    int numExcuse = 0;
+};
+
+// Function to register a user
+void registerUser (BasicCadets *cadets, int *index, int *numberOfCadets) {
+    cout << "User Number " << *index + 1 << ":" << endl;
+    cout << "User Name: ";
+    getline (cin, (cadets + *index)->userName);
+    cout << "Student ID: ";
+    getline (cin, (cadets + *index)->studentID);
+    cout << "Program: ";
+    getline (cin, (cadets + *index)->program);
+    cout << "Company: ";
+    getline (cin, (cadets + *index)->company);
+    cout << "Platoon: ";
+    getline (cin, (cadets + *index)->platoon);
+    *index++;
+    *numberOfCadets++;
+}
+
+
+int main (){
+    int numberOfCadets = 0; // total number of cadets registered
+    int lastCadetIndex = 0; // track the last index of user
+    BasicCadets *cadets = new BasicCadets[MAX_CADETS]; // dynamic array to store cadets
+
+    registerUser(cadets, &lastCadetIndex, &numberOfCadets); 
+
+    delete[] cadets;
     return 0;
 }
